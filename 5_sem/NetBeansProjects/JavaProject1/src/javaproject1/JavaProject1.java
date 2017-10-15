65ad1ac3eb470eef5fad6b925ab84d93f3572684
Danish Saeed <danishsaeed2@gmail.com>
2017-10-16 00:57:13 +0530

    bcd counter | code

---
+++ 5_sem/NetBeansProjects/JavaProject1/src/javaproject1/JavaProject1.java
@@ -0,0 +1,16 @@
+package javaproject1;
+
+/**
+ *
+ * @author danish
+ */
+public class JavaProject1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
